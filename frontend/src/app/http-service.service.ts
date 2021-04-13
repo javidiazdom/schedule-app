13a2ed8de8e25dc3ecf34a32cd084bcfb813356f
javidiazdom <javidiazdom@gmail.com>
2021-04-13 14:29:33 +0100

    Login component and style

---
+++ frontend/src/app/http-service.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {User} from './models/User';
+import {Routes} from './Routes';
+import {UserRegisterData} from './models/UserRegisterData'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http: HttpClient) { }
+
+  login(user: User) {
+    return this.http.post(Routes.login, user);
+  }
+  register(userResgisterData: UserRegisterData) {
+    return this.http.post(Routes.register, userResgisterData);
+  }
+}
